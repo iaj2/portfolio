b8e9c3fff7624eef745347ee2972f9dc0486e328
iaj2 <isaacjames124@yahoo.ca>
2023-08-31 19:27:24 -0700

    hero section stylez

--- app/src/components/heroSection/heroSection.jsx
+++ app/src/components/heroSection/heroSection.jsx
@@ -1,10 +1,46 @@
 import './heroSection.scss'
+import { ReactComponent as LinkedInIcon } from '../../assets/icons/linkedIn.svg'
+import { ReactComponent as GitHubIcon } from '../../assets/icons/github.svg'
+import { ReactComponent as RightArrowIcon } from '../../assets/icons/right-arrow.svg'
+import resumePdf from '../../assets/resume/9.1-Complex-numbers-PARTB.pdf'
+import meImg from '../../assets/images/me.jpg'
 
 const HeroSection = () => {
     return (
     <div className='hero-section-container'>
         <div className='hero-section-grid'>
-
+            <div className='intro'>
+                <p><span className='text-bold'>Hi,</span> I'm a driven computer science student with a solid foundation in software development and hands-on experience.</p>
+            </div>
+            <div className='image'>
+                <div className='img-back-drop'>
+                    <img src={meImg} alt=""></img>
+                </div>
+            </div>
+            <div className='school'>
+                <p>SFU Comp Sci BSc <span>• 2022-2026 • 3.97 GPA</span></p>
+            </div>
+            <div className='connect'>
+                <a href='https://www.linkedin.com/in/isaac-james-62765a278/' rel="noreferrer" target='_blank' className='social linkedIn'>
+                {<LinkedInIcon />}
+                </a>
+                <a href='https://github.com/iaj2' rel="noreferrer" target='_blank' className='social github'>
+                {<GitHubIcon />}
+                </a>
+                    
+                <button className='resume-button' id='resume'>
+                    <a href={resumePdf} rel="noreferrer" target='_blank' download={resumePdf}>RESUME</a>
+                </button>
+                <button className='contact-button' id='contact'>
+                    CONTACT
+                </button>
+            </div>
+            <div className='about-cta'>
+                <button className='about-cta-button' id='about-cta'>
+                    MORE ABOUT ME
+                    {<RightArrowIcon />}
+                </button>
+            </div>
         </div>
     </div>
     )
