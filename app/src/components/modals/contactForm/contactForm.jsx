c574d91ea6b99e70ee19634be7c50ecfb00de1e3
iaj2 <isaacjames124@yahoo.ca>
2023-09-07 18:23:16 -0700

    contact form stylezzzz

--- app/src/components/modals/contactForm/contactForm.jsx
+++ app/src/components/modals/contactForm/contactForm.jsx
@@ -16,11 +16,27 @@ export default function ContactFormModal() {
         {modal &&
           <div className='contact-form-modal'>
 			<div className='contact-form'>
-			<button className="close-button" onClick={toggleModal}>
-				{<CloseIcon />}
-			</button>
-				<p className="title">Let's Get Connected!</p>
-				
+				<button className="close-button" onClick={toggleModal}>
+					{<CloseIcon />}
+				</button>
+				<h2 className="title">Let's Get Connected!</h2>
+				<div className='name-container'>
+					<label for='name-input'>Name</label>
+					<input type='text' name='name' id='name-input'></input>
+				</div>
+				<div className='email-container'>
+					<label for='email-input'>Email</label>
+					<input type='email' name='email' id='email-input' required></input>
+				</div>
+				<div className='subject-container'>
+					<label for='subject-input'>Subject</label>
+					<input type='text' name='subject' id='subject-input'></input>
+				</div>
+				<div className='message-container'>
+					<label for='message-input'>Message</label>
+					<textarea name='message' id='message-input' className='message-input'></textarea>
+				</div>
+				<button className='submit-button'>SUBMIT</button>
 			</div>
         </div>
         }
